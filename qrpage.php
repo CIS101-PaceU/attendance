2aa813c994aa7c526058e5e6b5a56556c37beda4
Dax Patel <dp00174n@pace.edu>
2017-04-26 15:19:09 -0400

    Add files via upload
    updated file

--- qrpage.php
+++ qrpage.php
@@ -5,7 +5,8 @@ $input_text = $_POST['text'];
 $input_date = $_POST['date'];
 
 echo $input_text;
-//echo "<img src='qr_img.php?d=$input_date&?d=$input_text'/>";
+echo "<br>";
+echo "<img src='qr_img.php?d=$input_text'/>";
 ?>
 
 
