6321a1bbd992a1211423c7eaaf0646af64126054
daxpatel1 <dp00174n@pace.edu>
2017-04-26 15:08:31 -0400

    QR Code Generator
    The contains all the images and index page and arcade library

---
+++ php/qrpage.php
@@ -0,0 +1,15 @@
+<?php
+echo "Scan this code";
+print_r($_POST);
+$input_text = $_POST['text'];
+$input_date = $_POST['date'];
+
+echo $input_text;
+//echo "<img src='qr_img.php?d=$input_date&?d=$input_text'/>";
+?>
+
+
+
+
+
+
