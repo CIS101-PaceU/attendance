ec160de9757b46429652663de82b777a6edfe22f
daxpatel1 <dp00174n@pace.edu>
2017-05-01 18:56:51 -0400

    Small change to add date to generate qr code

--- php/qrpage.php
+++ php/qrpage.php
@@ -5,7 +5,8 @@ $input_text = $_POST['text'];
 $input_date = $_POST['date'];
 
 echo $input_text;
-//echo "<img src='qr_img.php?d=$input_date&?d=$input_text'/>";
+echo "<br>";
+echo "<center> <img src='qr_img.php?d=$input_text'&s=8&e=H/ height='600' width='600'> </center>";
 ?>
 
 
