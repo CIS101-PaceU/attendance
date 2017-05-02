a037c9d49b06f82f09ba0d10ba8693ffaa1e3c75
daxpatel1 <dp00174n@pace.edu>
2017-05-02 19:08:53 -0400

    A few minor changes of variation of the input

--- php/qrpage.php
+++ php/qrpage.php
@@ -1,14 +1,25 @@
-<?php
-echo "Scan this code";
-print_r($_POST);
-$input_text = $_POST['text'];
-$input_date = $_POST['date'];
+<!DOCTYPE html>
+<html>
 
-echo $input_text;
-echo "<br>";
-echo "<center> <img src='qr_img.php?d=$input_text'&s=8&e=H/ height='600' width='600'> </center>";
-?>
 
+<head>
+	<title>Generated QR code</title>
+</head>
+
+
+<body>
+	<?php
+		echo "Scan this code";
+		print_r($_POST);
+		$input_text = $_POST['text'];
+		$input_date = $_POST['date'];
+
+		echo $input_text;
+		echo "<br>";
+		echo "<center> <img src='qr_img.php?d=$input_text'&s=8&e=H/ height='600' width='600'> </center>";
+	?>
+</body>
+</html>
 
 
 
