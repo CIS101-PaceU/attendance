8135dcb2d4879c05ea974df6c03be201e34e4ab2
Dax Patel <dp00174n@pace.edu>
2017-05-03 12:03:06 -0400

    adding of date picker and making minutes slider

--- index.php
+++ index.php
@@ -4,7 +4,37 @@
 
 <head>
 	<title>Attendance</title>
+	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="style.css"/>
+  	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  	<link rel="stylesheet" href="/resources/demos/style.css">
+  	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  	<script>
+  		<!--date picker-->
+ 		$( function() 
+ 		{
+    		$( "#datepicker" ).datepicker();
+  		});
+ 		<!--slider-->
+  		$( function() 
+  		{
+    		$( "#slider" ).slider(
+    		{
+      		range: "min",
+      		min: 1,
+      		max: 20,
+      		value: 10,
+      		slide: function( event, ui ) 
+      		{
+        		$( "#amount" ).val( ui.value );
+      		}
+    		});
+    		$( "#amount" ).val( 
+    			$( "#slider" ).slider( "value" )
+    			);
+  		});
+  	</script>
 </head>
 
 
@@ -12,9 +42,10 @@
 
 	<h1 align ='center'>Attendace</h1>
 	<form action='qrpage.php' method='post' align='center'>
-		Date:       <input type='Date' name='date' id='date' value= <?php date() ?> ><br>
+		Date:      	 <input type='text' name='date' id='datepicker' ><br>
 		Event Name:  <input type='text' name='text' id='text'><br>
-		Active Time: <input type='time' name='time' id='time'><br>
+		Active Time: <input type='text' name='time' id='amount'><p>Minutes</p><br>
+ 		<div id='slider'></div>
 		<input type='submit' value='Submit'>
 	</form>
 
