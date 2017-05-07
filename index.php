07a714f4c8e580d446b8951f647e45f9d8ff8b9e
Dax Patel <dp00174n@pace.edu>
2017-05-07 17:52:42 -0400

    The adding of the pace template

--- index.php
+++ index.php
@@ -6,52 +6,62 @@
 	<title>Attendance</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="style.css"/>
-  	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  	<link rel="stylesheet" href="/resources/demos/style.css">
-  	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-  	<script>
-  		<!--date picker-->
+  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway" />
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+  	
+    <!--date picker-->
  		$( function() 
  		{
-    		$( "#datepicker" ).datepicker();
-  		});
- 		<!--slider-->
-  		$( function() 
-  		{
-    		$( "#slider" ).slider(
-    		{
-      		range: "min",
-      		min: 1,
-      		max: 20,
-      		value: 10,
-      		slide: function( event, ui ) 
-      		{
-        		$( "#amount" ).val( ui.value );
-      		}
-    		});
-    		$( "#amount" ).val( 
-    			$( "#slider" ).slider( "value" )
-    			);
-  		});
+    	$( "#datepicker" ).datepicker();
+  	});
+ 		
+    <!--slider-->
+  	$( function() 
+  	{
+    	$( "#slider" ).slider(
+    	{
+      	range: "min",
+      	min: 1,
+      	max: 20,
+      	value: 10,
+    		slide: function( event, ui ) 
+        {
+        	$( "#amount" ).val( ui.value );
+      	}
+    	});
+    	$( "#amount" ).val($( "#slider" ).slider( "value" ));
+  	});
   	</script>
 </head>
 
-
 <body>
-
-	<h1 align ='center'>Attendace</h1>
-	<form action='qrpage.php' method='post' align='center'>
-		Date:      	 <input type='text' name='date' id='datepicker' ><br>
-		Event Name:  <input type='text' name='text' id='text'><br>
-		Active Time: <input type='text' name='time' id='amount'><p>Minutes</p><br>
- 		<div id='slider'></div>
-		<input type='submit' value='Submit'>
-	</form>
-
-
-
+  <div id="contents">
+    <h1>Attendance</h1>
+      <form action='qrpage.php' method='post'><br>
+        <p>Date:</p>        <input type='text' name='date' id='datepicker' ><br>
+        <p>Event Name:</p>  <input type='text' name='text' id='text'><br>
+        <p>Active Time:</p>
+        <div id='slider'>
+          <br>
+          <input type='text' name='time' id='amount' maxlength="2" size="12"><label>Minutes</label>
+        </div>
+        <br>
+        <br>
+        <input type='submit' value='Submit'>
+      </form>
+  </div>
+  <div id="nav">
+    <img src="img_Pace_logo.png" alt="Pace Univercity Logo" style="width:80%"><br>
+    <h2><b>CIS 101 Portal</b></h2>
+    <a href="index.php">Attendance</a><br>
+  </div>
 </body>
+
 </html>
 
 
--- qrpage.php
+++ qrpage.php
@@ -4,20 +4,55 @@
 
 <head>
 	<title>Generated QR code</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway" />
+  	<script language="javascript" type="text/javascript" src="libraries/p5.js"></script>
+ 	<script language="javascript" src="libraries/p5.dom.js"></script>
+  	<script language="javascript" src="libraries/p5.sound.js"></script>
+  	<script language="javascript" type="text/javascript" src="sketch.js"></script>
+	<script type="text/javascript">
+	
+	var counter = 0;
+	function setup()
+	{
+		noCanvas();
+
+		var timer = select('#timer');
+		timer.html('counter');
+
+		fucntion timeIT()
+		{
+			counter++;
+			timer.html(counter);
+		}
+
+		setInterval(timeIt, 1000);
+
+	}
+
+
+
+	</script>
 </head>
 
 
 <body>
-	<?php
-		echo "Scan this code";
-		print_r($_POST);
-		$input_text = $_POST['text'];
-		$input_date = $_POST['date'];
+	<div id="contents_qrpage">
+	<h1 id="timer">00:00</h1><br><br>
+		<?php
+			echo "<center> <img src='qr_img.php?d=$input_text height='500' width='500'> </center>";
+		?>
+	</div>
 
-		echo $input_text;
-		echo "<br>";
-		echo "<center> <img src='qr_img.php?d=$input_text'&s=8&e=H/ height='600' width='600'> </center>";
-	?>
+	<div id="nav">
+		<img src="img_Pace_logo.png" alt="Pace Univercity Logo" style="width:80%"><br>
+		<h2><b>CIS 101 Portal</b></h2>
+  		<a href="index.php">Attendance</a><br>
+	</div>	
+
+
+	
 </body>
 </html>
 
