431619ccf017b06ca8f27788f48320093fd65ec6
daxpatel1 <dp00174n@pace.edu>
2017-10-09 23:10:48 -0400

    added qrcode

--- php/index.php
+++ php/index.php
@@ -6,52 +6,73 @@
 	<title>Attendance</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="style.css"/>
-  	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  	<link rel="stylesheet" href="/resources/demos/style.css">
-  	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-  	<script>
-  		<!--date picker-->
- 		$( function() 
- 		{
-    		$( "#datepicker" ).datepicker();
-  		});
- 		<!--slider-->
-  		$( function() 
-  		{
-    		$( "#slider" ).slider(
-    		{
-      		range: "min",
-      		min: 1,
-      		max: 20,
-      		value: 10,
-      		slide: function( event, ui ) 
-      		{
-        		$( "#amount" ).val( ui.value );
-      		}
-    		});
-    		$( "#amount" ).val( 
-    			$( "#slider" ).slider( "value" )
-    			);
-  		});
-  	</script>
+  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway" />
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    
+    <script>
+      <!--date picker-->
+ 	    $( function() 
+ 	    {
+    	 $( "#datepicker" ).datepicker();
+  	  });
+ 		
+      <!--slider-->
+  	   $( function() 
+  	   {
+    	   $( "#slider" ).slider(
+    	 {
+      	 range: "min",
+      	 min: 1,
+      	 max: 20,
+      	 value: 10,
+    		  slide: function( event, ui ) 
+          {
+        	 $( "#amount" ).val( ui.value );
+      	  }
+    	 });
+    	 $( "#amount" ).val($( "#slider" ).slider( "value" ));
+  	   });
+  </script>
+
 </head>
 
-
 <body>
-
-	<h1 align ='center'>Attendace</h1>
-	<form action='qrpage.php' method='post' align='center'>
-		Date:      	 <input type='text' name='date' id='datepicker' ><br>
-		Event Name:  <input type='text' name='text' id='text'><br>
-		Active Time: <input type='text' name='time' id='amount'> Minutes<br>
- 		<div id='slider'></div>
-		<input type='submit' value='Submit'>
-	</form>
-
-
-
+  <div id="contents">
+    <h1>Attendance</h1>
+      <form action='qrpage.php' method='post'><br>
+        <p>Date:  <input type='text' name='date' id='datepicker' readonly="readonly"><br><br><br><br></p>
+        <p>Event Name: 
+          <select type='text' name='text' id='text'>
+            <option value="1">Week 1</option>
+            <option value="2">Week 2</option>
+            <option value="3">Week 3</option>
+            <option value="4">Week 4</option>
+            <option value="5">Week 5</option>
+            <option value="6">Week 6</option>
+            <option value="7">Week 7</option>
+          </select>
+          <br><br><br><br></p>
+        <p>Active Time:
+          <div id='slider'>
+            <br>
+            <input name='time' id='amount' maxlength="2" size="2" readonly="readonly"><label>Minutes</label>
+          </div>
+        </p>
+        <br><br><br><br>
+        <input type='submit' value='Submit' readonly="readonly">
+      </form>
+  </div>
+  <div id="nav">
+    <img src="img_Pace_logo.png" alt="Pace Univercity Logo" style="width:80%"><br>
+    <h2><b>CIS 101 Portal</b></h2>
+    <a href="index.php">Attendance</a><br>
+  </div>
 </body>
+
 </html>
 
 
--- php/qrpage.php
+++ php/qrpage.php
@@ -4,20 +4,61 @@
 
 <head>
 	<title>Generated QR code</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway" />
+	
+	
+	<script type="text/javascript">
+
+		var seconds = <?php echo $_POST['time'] ?>*60; //converting min to sec
+      		function secondPassed() 
+      		{
+          		var minutes = Math.round((seconds - 30)/60),
+              	remainingSeconds = seconds % 60;
+              	if (remainingSeconds < 10) 
+              	{
+              		remainingSeconds = "0" + remainingSeconds;
+              		time =1;
+          		}
+          		document.getElementById('countdown').innerHTML = minutes + ":" + remainingSeconds;
+          		if (seconds == 0) 
+          		{
+              		document.getElementById('countdown').innerHTML = "Times Up!!";
+              		//document.getElementById('image').innerHTML = "<img src="Late_Submission" + retvalue + ".png")";
+              	} 
+              	else 
+              	{
+              		seconds--;
+          		}
+      		}
+      	var countdownTimer = setInterval('secondPassed()', 1000);
+      
+	</script>
 </head>
 
-
 <body>
-	<?php
-		echo "Scan this code";
-		print_r($_POST);
-		$input_text = $_POST['text'];
-		$input_date = $_POST['date'];
+	<div id="nav">
+		<img src="img_Pace_logo.png" alt="Pace Univercity Logo" style="width:80%"><br>
+		<h2><b>CIS 101 Portal</b></h2>
+  		<a href="index.php">Attendance</a><br>
+	</div>	
 
-		echo $input_text;
-		echo "<br>";
-		echo "<center> <img src='qr_img.php?d=$input_text'&s=8&e=H/ height='600' width='600'> </center>";
-	?>
+	<div id="contents_qrpage">
+			<div id="timer">
+				<h1><time id="countdown"><?php echo $_POST['time']?>:00</time></h1>
+			</div>
+		<div id="image">
+			<?php
+				
+				$input_text = $_POST['text'];
+				$input_date = $_POST['date'];
+				$minutes = $_POST['time'];
+				echo "<br> <br> <img src='qr_img.php?d=$input_text' height='500' width='500' align='center'>";
+			?>
+		</div>
+	</div>
+	
 </body>
 </html>
 
